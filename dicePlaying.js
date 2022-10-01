ef0b71dcd65e301baf6c5f3eeb3574ed5ff5aa61
dm1itri <dimma-18@yandex.ru>
2022-10-01 21:22:12 +0300

    каждый игрок ходит по своей трактории

--- dicePlaying.js
+++ dicePlaying.js
@@ -8,6 +8,12 @@ const diceId = [
 ];
 
 let firstPlayerCoords = 0
+let secondPlayerCoords = 0
+let thirdPlayerCoords = 0
+let fourthPlayerCoords = 0
+const playersCoords = [firstPlayerCoords, secondPlayerCoords, thirdPlayerCoords, fourthPlayerCoords]
+let currentPlayer = 0
+
 
 function randomIntFromInterval(min, max) { // min and max included
   return Math.floor(Math.random() * (max - min + 1) + min)
@@ -38,12 +44,19 @@ function dicePlaying() {
             document.getElementById(diceId[i]).style.display = 'none'
             document.getElementById('rollDice').style.display = 'block'
         }
-        rollDice(firstPlayerCoords, currentIndex + 1)
+        rollDice(currentPlayer, playersCoords[currentPlayer], currentIndex + 1)
     }, 3000)
 }
 
-function rollDice (index_0, number_steps) {
+function rollDice (numberPlayer, index_0, number_steps) {
     const square_cards = [0, 7, 12, 19]
+    const playerId = ['firstPlayer', 'secondPlayer', 'thirdPlayer', 'fourthPlayer']
+    let coords
+    //const pos_x0 = [200, 100, 200]
+    //const pos_y0 = [200, 100, 200]
+    //const pos_x1 = [100, 100, 200]
+    //const pos_y1 = [200, 100, 200]
+    let move
     if (index_0 >= 19) {
         index_0 -= 19
     } else if (index_0 >= 12) {
@@ -51,36 +64,107 @@ function rollDice (index_0, number_steps) {
     } else if (index_0 >= 7) {
         index_0 -=7
     }
-    for (let i = 1; i <= number_steps; i++) {
+/*    for (let i = 1; i <= number_steps; i++) {
         if (0 <= firstPlayerCoords && firstPlayerCoords < 7) {
-            document.getElementById('firstPlayer').style.left = index_0  * 100 + 200 + 'px'
+            document.getElementById(playerId[numberPlayer]).style.left = index_0  * 100 + 200 + 'px'
         } else if (7 <= firstPlayerCoords && firstPlayerCoords < 12) {
-            document.getElementById('firstPlayer').style.top = index_0 * 100 + 200 + 'px'
+            document.getElementById(playerId[numberPlayer]).style.top = index_0 * 100 + 200 + 'px'
         } else if (12 <= firstPlayerCoords && firstPlayerCoords < 19) {
             if (firstPlayerCoords !== 18){
-                document.getElementById('firstPlayer').style.left = 800 - (index_0 * 100 + 100) + 'px'
+                document.getElementById(playerId[numberPlayer]).style.left = 800 - (index_0 * 100 + 100) + 'px'
             } else {
-                document.getElementById('firstPlayer').style.left = 800 - (index_0 * 100 + 200) + 'px'
+                document.getElementById(playerId[numberPlayer]).style.left = 800 - (index_0 * 100 + 200) + 'px'
             }
-        }
-        else {
+        } else {
             if (firstPlayerCoords !== 23){
-                document.getElementById('firstPlayer').style.top = 600 - (index_0 * 100 + 100) + 'px'
+                document.getElementById(playerId[numberPlayer]).style.top = 600 - (index_0 * 100 + 100) + 'px'
             } else {
-                document.getElementById('firstPlayer').style.top = 600 - (index_0 * 100 + 200) + 'px'
+                document.getElementById(playerId[numberPlayer]).style.top = 600 - (index_0 * 100 + 200) + 'px'
+            }
+        }*/
+    for (let i = 1; i <= number_steps; i++) {
+        coords = playersCoords[numberPlayer]
+        if (numberPlayer === 0) {
+            if (0 <= coords && coords < 7 || 12 <= coords && coords < 19) {
+                if (0 <= coords && coords< 7) {
+                    move =  index_0  * 100 + 200
+                } else {
+                    move = coords !== 18 ? 700 - index_0 * 100 : 700 - index_0 * 100 - 100
+                }
+                document.getElementById(playerId[numberPlayer]).style.left = move + 'px'
+            } else {
+                if (7 <= coords && coords < 12) {
+                    move = index_0 * 100 + 200
+                } else {
+                    move = coords !== 23 ? 500 - index_0 * 100 : move = 500 - index_0 * 100 - 100
+                }
+                document.getElementById(playerId[numberPlayer]).style.top = move + 'px'
+            }
+        } else if (numberPlayer === 1) {
+            if (0 <= coords && coords < 7 || 12 <= coords && coords < 19) {
+                if (0 <= coords && coords < 7) {
+                    move =  coords !== 6 ? index_0  * 100 + 250 : index_0  * 100 + 350
+                } else {
+                    move =  750 - index_0 * 100
+                }
+                document.getElementById(playerId[numberPlayer]).style.left = move + 'px'
+            } else {
+                if (7 <= coords && coords < 12) {
+                    move = index_0 * 100 + 200
+                } else {
+                    move = coords !== 23 ? 500 - index_0 * 100: move = 500 - index_0 * 100 - 100
+                }
+                document.getElementById(playerId[numberPlayer]).style.top = move + 'px'
+            }
+        } else if (numberPlayer === 2) {
+            if (0 <= coords && coords < 7 || 12 <= coords && coords < 19) {
+                if (0 <= coords && coords < 7) {
+                    move =  index_0  * 100 + 200
+                } else {
+                    move = coords !== 18 ? 700 - index_0 * 100 : 700 - index_0 * 100 - 100
+                }
+                document.getElementById(playerId[numberPlayer]).style.left = move + 'px'
+            } else {
+                if (7 <= coords && coords < 12) {
+                    move = coords !== 11 ? index_0 * 100 + 250 : index_0 * 100 + 350
+                } else {
+                    move =  550 - index_0 * 100
+                }
+                document.getElementById(playerId[numberPlayer]).style.top = move + 'px'
+            }
+        } else if (numberPlayer === 3) {
+            if (0 <= coords && coords < 7 || 12 <= coords && coords < 19) {
+                if (0 <= coords && coords < 7) {
+                    move =  coords !== 6 ? index_0  * 100 + 250 : index_0  * 100 + 350
+                } else {
+                    move =  750 - index_0 * 100
+                }
+                document.getElementById(playerId[numberPlayer]).style.left = move + 'px'
+            } else {
+                if (7 <= coords && coords < 12) {
+                    move = coords !== 11 ? index_0 * 100 + 250 : index_0 * 100 + 350
+                } else {
+                    move =  550 - index_0 * 100
+                }
+                document.getElementById(playerId[numberPlayer]).style.top = move + 'px'
             }
         }
 
         index_0 ++
-        firstPlayerCoords ++
-        if (firstPlayerCoords === 24) {
-            firstPlayerCoords = 0
+        playersCoords[numberPlayer] ++
+        if (playersCoords[numberPlayer] === 24) {
+            playersCoords[numberPlayer] = 0
         }
-        if (square_cards.indexOf(firstPlayerCoords) !== -1) {
+        if (square_cards.indexOf(playersCoords[numberPlayer]) !== -1) {
             index_0 = 0
         }
     }
+    currentPlayer = (currentPlayer + 1) % 4
+
+
+
+    }
     //alert(firstPlayerCoords)
-}
+
 
 //rollDice(firstPlayerCoords, 16)
